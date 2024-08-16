53fa44268f8f73327951f2302442b9642735c973
yohjimcode <collignon.mickael1987@gmail.com>
2024-08-16 23:00:48 +0200

    config application.properties

---
+++ backEnd/src/test/java/com/EditionLivre/EditionLivreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EditionLivre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EditionLivreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
